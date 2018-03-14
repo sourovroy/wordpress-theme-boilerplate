e0b763e0ffbde5f25f45d591425fcffc2705b17b
Anjan Biswas <anjan@nextdot.com.au>
2018-03-14 15:14:09 +0600

    Windows 95% emitting issue fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.js('assets/js/custom.js', 'assets/public/scripts.js');
-
-mix.sass('assets/css/custom.scss', 'assets/public/style.css');
+mix.js('assets/js/custom.js', 'assets/public/scripts.js')
+	.sass('assets/css/custom.scss', 'assets/public/style.css')
+	.setPublicPath('assets/public');
