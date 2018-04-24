a5271c0a10c8a77c3c77861a8cc13e15bbb6468e
Sourov Roy <sourov@nextdot.com.au>
2018-04-24 09:12:13 +0600

    Sourcemap code added to webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 let mix = require('laravel-mix');
 
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'source-map'
+    })
+    .sourceMaps()
+}
+
 mix.js('assets/js/custom.js', 'assets/public/scripts.js')
 	.sass('assets/css/custom.scss', 'assets/public/style.css')
 	.setPublicPath('assets/public')
